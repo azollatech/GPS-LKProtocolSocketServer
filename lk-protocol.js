aa699521b16ad61605b02b2f5006f7f13fc64622
henneth <henneth.cheng@gmail.com>
2018-04-12 16:21:13 +0800

    raw data

---
+++ lk-protocol.js
@@ -0,0 +1,15 @@
+var server = require ('tk102');
+
+// start server
+server.createServer ({
+    port: 1337
+});
+
+// incoming data, i.e. update a map
+// server.on ('track', function (gps) {
+//   updateMap (gps.geo.latitude, gps.geo.longitude);
+// });
+
+server.on ('data', function (raw) {
+    console.log ('Incoming data: '+ raw);
+});
